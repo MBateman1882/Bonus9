fcf0f316a5e5cc48519b06907f17577e65c73fdc
MattBateman <mbatem1882@yahoo.com>
2020-01-14 20:03:09 -0500

    first commit

---
+++ Bonus9/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bonus9.Models;
+
+namespace Bonus9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //by adding a new action I can load a new view
+        public IActionResult About()
+        {
+            //return view here, if view isn't specified it defaults to the Action Name
+            return View();
+        }
+
+        //add a portfolio action
+        public IActionResult Portfolio()
+        {
+            //return our portfolio view
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
